e097b20f78ba911ae02e3e69536a9ce1d9c54b1f
Jelle Tempelman <j.tempelman@jelletempelman.nl>
2017-03-08 16:55:11 +0100

    no message

---
+++ js/mastermind.js
@@ -0,0 +1,16 @@
+// geheime kleuren: kleurcode kan zijn 1, 2, 3, 4, 5, 6
+
+var geheimekleur_1 = 1+ Math.floor((Math.random() * 6));
+var geheimekleur_2 = 1+ Math.floor((Math.random() * 6));
+var geheimekleur_3 = 1+ Math.floor((Math.random() * 6));
+var geheimekleur_4 = 1+ Math.floor((Math.random() * 6));
+
+console.log(geheimekleur_1);
+console.log(geheimekleur_2);
+console.log(geheimekleur_3);
+console.log(geheimekleur_4);
+
+function Raad(){
+    var raad1 = document.getElementById()
+
+}
